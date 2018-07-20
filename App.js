be20d59b9a5e81b6fee55fd79caaa904666324a5
huzaifahsulaiman <huzaifah1008@gmail.com>
2018-07-20 06:12:11 -0400

    Start Screen, RoundedButton

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoggedOut from './src/screens/LoggedOut';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <LoggedOut/>
+    );
+  }
+}
