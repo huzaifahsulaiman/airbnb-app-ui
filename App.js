8270fee4afa03aeac22aa687e30487b159455a8a
huzaifahsulaiman <huzaifah1008@gmail.com>
2018-07-20 18:57:07 -0400

    Login Screen, Next arrow Button

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import LoggedOut from './src/screens/LoggedOut';
+import LogIn from './src/screens/LogIn';
 
 export default class App extends React.Component {
   render() {
     return (
-      <LoggedOut/>
+      // <LoggedOut/>
+      <LogIn/>
     );
   }
 }
