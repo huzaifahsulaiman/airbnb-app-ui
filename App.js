6add0521c5b5a9c46afbef1f07eb03d4f84a3aaf
huzaifahsulaiman <huzaifah1008@gmail.com>
2018-07-23 19:11:44 -0400

    Forgot Password screen, components are used from Login Screen

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import LoggedOut from './src/screens/LoggedOut';
 import LogIn from './src/screens/LogIn';
+import ForgotPassword from './src/screens/ForgotPassword';
 
 export default class App extends React.Component {
   render() {
     return (
       // <LoggedOut/>
-      <LogIn/>
+      // <LogIn/>
+      <ForgotPassword/>
     );
   }
 }
